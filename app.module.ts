d27a8f6c271e08ce5ff316260b6baef594c857ec
zyarr-technologies <78398850+zyarr-technologies@users.noreply.github.com>
2021-02-03 07:13:09 -0800

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { BooklistComponent } from './booklist/booklist.component';
+import { BookdetailComponent } from './bookdetail/bookdetail.component';
+import {HttpClientModule} from '@angular/common/http';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomeComponent,
+    BooklistComponent,
+    BookdetailComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
